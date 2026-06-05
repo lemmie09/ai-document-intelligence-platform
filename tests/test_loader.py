73dc79eeb78d7ccccf9854f73d98a4fe36662a7d
Lemmie Carvalho <lemmiecarvalho@gmail.com>
2026-06-04 23:14:25 -0700

    Test PDF text extraction

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,3 +1,7 @@
 from src.ingestion.pdf_loader import PDFLoader
 
 loader = PDFLoader()
+
+text = loader.load("/Users/lemmiecarvalho/Downloads/reference paper/all papers/paper3.pdf")
+
+print(text[:500])
